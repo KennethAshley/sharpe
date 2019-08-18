70302ce4f722a17890badeabb5f494d7660c5abd
Kenneth Ashley <ken.u.diggitt@gmail.com>
2019-08-18 02:39:28 -0400

    Add table

---
+++ src/components/Table/Table.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Table from './Table';
+
+interface List {
+  currency: string;
+  prices: number[];
+  sharpeRatio: number;
+	avgDailyReturn: number;
+};
+
+const currencies = [
+  {
+    currency: 'BTC',
+    prices: [10000, 20000],
+    sharpeRatio: 0.999,
+    avgDailyReturn: 0.50,
+  },
+  {
+    currency: 'ETH',
+    prices: [100, 200],
+    sharpeRatio: 0.999,
+    avgDailyReturn: 0.50,
+  }
+];
+
+it('renders the Table component without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Table currencies={currencies} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+// other test would include testing on click of button (sorting)
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import * as math from 'mathjs';
+
+import { sort } from '../../utils';
+
+interface Currencies {
+  order: string;
+  list: List[];
+}
+
+interface List {
+  currency: string;
+  prices: number[];
+  sharpeRatio: number;
+	avgDailyReturn: number;
+};
+
+interface TableProps {
+  currencies: List[];
+};
+
+function Table(props: TableProps) {
+  const [currencies, setCurrencies] = useState<Currencies>({ order: '', list: [] })
+
+  useEffect(() => {
+    const adjustedCurrencies = props.currencies.map(currency => {
+			const sharpeRatio = (math.mean(currency.prices) / math.std(currency.prices));
+			const avgDailyReturn = currency.prices.reduce((acc: number, val: number) => acc + Number(val), 0);
+
+      return {
+        ...currency,
+        sharpeRatio,
+				avgDailyReturn: avgDailyReturn / currency.prices.length,
+      };
+    });
+
+    setCurrencies({
+      order: '',
+      list: adjustedCurrencies
+    });
+  }, [ props.currencies ]);
+
+  function onSort(label: string, order: string): void {
+    const sortedCurrencies = sort(label, order, currencies.list);
+
+    setCurrencies({
+      order,
+      list: sortedCurrencies
+    });
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>
+							<div className="th-content">
+								Currency
+								<div>
+									<button onClick={() => onSort('currency', 'asc')}>
+										<small>
+											ASC
+										</small>
+									</button>
+									<button onClick={() => onSort('currency', 'desc')}>
+										<small>
+											DESC
+										</small>
+									</button>
+								</div>
+							</div>
+            </th>
+            <th>
+							<div className="th-content">
+								Reward to Ratio
+								<div className="th-actions">
+									<button onClick={() => onSort('sharpeRatio', 'asc')}>
+										<small>
+											ASC
+										</small>
+									</button>
+									<button onClick={() => onSort('sharpeRatio', 'desc')}>
+										<small>
+											DESC
+										</small>
+									</button>
+								</div>
+							</div>
+            </th>
+            <th>Avg. Daily Return</th>
+          </tr>
+        </thead>
+        <tbody>
+          { currencies.list.map((currency, index) => (
+              <tr key={`currency_${index}`}>
+                <td>{currency.currency}</td>
+                <td>{currency.sharpeRatio}</td>
+                <td>{currency.avgDailyReturn}</td>
+              </tr>
+          )) }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
+
